eeb3b2d2174481f7a25190c4b5cb4aaa52268f8f
joey.blair <joeblair@udel.edu>
2023-03-02 02:55:39 -0500

    makeAnswers and publishAll

--- src/nested.ts
+++ src/nested.ts
@@ -37,7 +37,7 @@ export function findQuestion(
     questions: Question[],
     id: number
 ): Question | null {
-    const found = questions.find((questions) => questions.id === id);
+    const found = questions.find((question) => question.id === id);
     if (<Question>found) {
         return found;
     }
@@ -128,7 +128,15 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    const answers: Answer[] = questions.map(
+        (question: Question): Answer => ({
+            questionId: question.id,
+            text: "",
+            submitted: false,
+            correct: false
+        })
+    );
+    return answers;
 }
 
 /***
@@ -136,7 +144,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    const published = questions.map(
+        (question: Question): Question => ({ ...question, published: true })
+    );
+    return published;
 }
 
 /***
