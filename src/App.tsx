af00a878cf0797b7f556695472596ec25de82901
joey.blair <joeblair@udel.edu>
2023-03-07 17:05:39 -0500

    Counter and revealAnswer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import pikachu from "../src/pikachu.jpg";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -11,38 +9,13 @@ import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-                <h1>This is a header.</h1>
-            </header>
+        <div>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reloa. Joey Blair and Hello World
+                Counter <Counter></Counter>
             </p>
             <p>
-                <ol>
-                    <li>First Item</li>
-                    <li>Second Item</li>
-                    <li>Third Item</li>
-                </ol>
+                <RevealAnswer></RevealAnswer>
             </p>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <p>
-                <img src={pikachu} alt="Pikachu" />
-            </p>
-            <Container>
-                <Row>
-                    <Col>
-                        Red Rectangle One.<div className="rectangle"></div>
-                    </Col>
-                    <Col>
-                        Red Rectangle Two.<div className="rectangleTwo"></div>
-                    </Col>
-                </Row>
-            </Container>
         </div>
     );
 }
